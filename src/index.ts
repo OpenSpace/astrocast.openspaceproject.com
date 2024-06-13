91845e4109a07e6552dc1dbc95b6551fee97284a
Alexander Bock <alexander.bock@liu.se>
2024-06-13 17:17:28 +0200

    Update the Wormhole to make it compile in Typescript again

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  *                                                                                       *
  * Wormhole                                                                              *
  *                                                                                       *
- * Copyright (c) 2022                                                                    *
+ * Copyright (c) 2022-2024                                                               *
  *                                                                                       *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
