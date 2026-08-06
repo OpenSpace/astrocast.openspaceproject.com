a958fd36fb08babfd7c1d9d60f385ecc27fc8abd
Andreas Engberg <adde.engberg@gmail.com>
2026-08-06 16:45:25 +0200

    Fix burger menu not working

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -7,7 +7,7 @@ import { AdminIcon, HomeIcon } from '@/icons/icons';
 import { Profile } from './Profile';
 
 export function AppLayout() {
-  const [opened, { toggle }] = useDisclosure();
+  const [opened, { toggle, close }] = useDisclosure();
   return (
     <AppShell
       padding="xs"
@@ -16,30 +16,39 @@ export function AppLayout() {
     >
       <AppShell.Header>
         <Group h={'100%'} justify={'space-between'} px={'xs'}>
-          <Link to={'/'} style={{ textDecoration: 'none', color: 'inherit' }}>
-            <Group gap={'xs'}>
-              <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size={'sm'} />
-              <Image
-                src={'/images/icon.png'}
-                alt={'OpenSpace logo'}
-                height={40}
-                w={'auto'}
-                fit={'contain'}
-                display={'inline-block'}
-              />
-              <Title order={1}>Astrocast</Title>
-            </Group>
-          </Link>
+          <Group gap={'xs'}>
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size={'sm'} />
+            <Link to={'/'} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <Group gap={'xs'}>
+                <Image
+                  src={'/images/icon.png'}
+                  alt={'OpenSpace logo'}
+                  height={40}
+                  w={'auto'}
+                  fit={'contain'}
+                  display={'inline-block'}
+                />
+                <Title order={1}>Astrocast</Title>
+              </Group>
+            </Link>
+          </Group>
           <Profile />
         </Group>
       </AppShell.Header>
       <AppShell.Navbar>
-        <NavLink component={Link} to={'/'} label={'Home'} leftSection={<HomeIcon />} />
+        <NavLink
+          component={Link}
+          to={'/'}
+          label={'Home'}
+          leftSection={<HomeIcon />}
+          onClick={close}
+        />
         <NavLink
           component={Link}
           to={'/admin'}
           label={'Admin'}
           leftSection={<AdminIcon />}
+          onClick={close}
         />
       </AppShell.Navbar>
       <AppShell.Main>
