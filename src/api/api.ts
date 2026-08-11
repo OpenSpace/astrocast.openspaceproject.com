6bbc799eb934739acd3d69cbb2595caf20d947e2
Andreas Engberg <adde.engberg@gmail.com>
2026-08-11 14:01:21 +0200

    potential fix for data race when reconnecting api

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,11 @@ import OpenSpaceApi from 'openspace-api-js';
 export let api = OpenSpaceApi('localhost', 4682);
 
 export function initApi(address: string, port: number) {
+  // Clear any old instance handers first. Making any pending or in-flight connect/close
+  // event from the discarded instance no-op
+  api.onConnect(() => {});
+  api.onDisconnect(() => {});
+
   api.disconnect();
   api = OpenSpaceApi(address, port);
 }
